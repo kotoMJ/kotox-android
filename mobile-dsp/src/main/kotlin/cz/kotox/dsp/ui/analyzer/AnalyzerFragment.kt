98030df966b24a24557d02768fe318c050fe2b03
Michal <jenicek.michal@gmail.com>
2020-02-04 12:06:06 +0100

    Add permission handling.

--- mobile-dsp/src/main/kotlin/cz/kotox/dsp/ui/analyzer/AnalyzerFragment.kt
+++ mobile-dsp/src/main/kotlin/cz/kotox/dsp/ui/analyzer/AnalyzerFragment.kt
@@ -1,14 +1,21 @@
 package cz.kotox.dsp.ui.analyzer
 
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.navigation.Navigation
 import cz.kotox.core.arch.BaseFragmentViewModel
+import cz.kotox.core.utility.FragmentPermissionManager
+import cz.kotox.core.utility.lazyUnsafe
 import cz.kotox.dsp.R
 import cz.kotox.dsp.databinding.AnalyzerFragmentBinding
 
 class AnalyzerFragment : BaseFragmentViewModel<AnalyzerViewModel, AnalyzerFragmentBinding>() {
+
+	val permissionManager: FragmentPermissionManager by lazyUnsafe {
+		FragmentPermissionManager(this)
+	}
 
 	override fun inflateBindingLayout(inflater: LayoutInflater) = AnalyzerFragmentBinding.inflate(inflater)
 
@@ -19,10 +26,29 @@ class AnalyzerFragment : BaseFragmentViewModel<AnalyzerViewModel, AnalyzerFragme
 		lifecycle.addObserver(viewModel)
 	}
 
+	override fun onResume() {
+		super.onResume()
+
+		if (!permissionManager.checkRecordAudioPermission()) {
+			permissionManager.requestRecordAudioPermissions({}, {
+				showToast("RecordAudio permissions is required for audio analysis")
+				activity?.finish()
+			})
+		}
+	}
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		binding.navigateProcessingBt.setOnClickListener {
 			Navigation.findNavController(view).navigate(R.id.navigate_to_processing)
 		}
+	}
+
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+		permissionManager.onPermissionResult(requestCode, permissions, grantResults)
+	}
+
+	private fun checkAudioPermissions() {
+
 	}
 }
