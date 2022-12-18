460a5cc2c2fb81c3859c28f89de7b12db396b284
Michal <jenicek.michal@gmail.com>
2022-12-18 17:20:03 +0100

    Prepare data holder to show latest camera uri.

--- mobile-kotox-media/src/main/kotlin/cz/kotox/media/ui/MainViewModel.kt
+++ mobile-kotox-media/src/main/kotlin/cz/kotox/media/ui/MainViewModel.kt
@@ -1,5 +1,11 @@
 package cz.kotox.media.ui
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +15,12 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
 ) : ViewModel() {
 
+    private val latestPhotoUri: MutableState<String?> = mutableStateOf(null)
+
+    val latestPhotoUriPresenter = derivedStateOf { latestPhotoUri.value }
+
+    fun updateLatestPhotoUri(photoUri: String) {
+        latestPhotoUri.value = photoUri
+    }
 
 }
