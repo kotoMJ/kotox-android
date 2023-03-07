9c87c8b2f5786910f1064a2112acf540ac1ef8eb
Michal <jenicek.michal@gmail.com>
2023-03-07 12:11:36 +0100

    Remove redundant code to reduce complexity.

--- build-logic/convention/src/main/kotlin/cz/kotox/android/poeditor/xml/Constants.kt
+++ build-logic/convention/src/main/kotlin/cz/kotox/android/poeditor/xml/Constants.kt
@@ -2,7 +2,4 @@ package cz.kotox.android.poeditor.xml
 
 import org.gradle.api.logging.Logging
 
-const val ALL_REGEX_STRING = """^(.+)$"""
-
-
 val logger = Logging.getLogger(org.gradle.api.logging.Logger::class.java)!!
