bc47027982c36692cb1ab8dfa0b25cb699c718e6
Michal <jenicek.michal@gmail.com>
2023-03-07 14:49:04 +0100

    Simplify xml processor.

--- build-logic/convention/src/main/kotlin/cz/kotox/android/poeditor/xml/XmlPostProcessor.kt
+++ build-logic/convention/src/main/kotlin/cz/kotox/android/poeditor/xml/XmlPostProcessor.kt
@@ -1,7 +1,5 @@
 package cz.kotox.android.poeditor.xml
 
-import cz.kotox.android.poeditor.getModuleName
-import org.w3c.dom.CDATASection
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
@@ -15,16 +13,12 @@ import org.w3c.dom.NodeList
 class XmlPostProcessor {
     companion object {
         private val DEFAULT_ENCODING = Charsets.UTF_8
-        private val VARIABLE_REGEX = Regex("""\{\d?\{(.*?)\}\}""")
 
         private const val TAG_RESOURCES = "resources"
         private const val TAG_STRING = "string"
         private const val TAG_PLURALS = "plurals"
         private const val TAG_ITEM = "item"
 
-        private const val ATTR_NAME = "name"
-        private const val ATTR_NAME_IOS_DELIMITER = "."
-        private const val ATTR_NAME_ANDROID_DELIMITER = "."
     }
 
 
@@ -32,11 +26,10 @@ class XmlPostProcessor {
         // Parse line by line by traversing the original file using DOM
         val translationFileXmlDocument = translationFileXmlString.toStringsXmlDocument()
 
-        val elementMap = formatTranslationXmlDocument(
+        val elementMap = processAndCategorizeXmlElements(
             translationFileXmlDocument,
             translationFileXmlDocument.childNodes
         )
-
 
         return elementMap.map { (key, value) ->
 
@@ -53,40 +46,12 @@ class XmlPostProcessor {
 
     }
 
-    /**
-     * Formats a given string to conform to Android strings.xml format.
-     */
-    fun formatTranslationString(translationString: String): String {
-        // We need to check for variables to see if we have to escape percent symbols: if we find variables, we have
-        // to escape them
-        val containsVariables = translationString.contains(VARIABLE_REGEX)
-
-        val placeholderTransform: (MatchResult) -> CharSequence = { matchResult ->
-            // Pending: if the string has multiple variables but any of them has no order number,
-            //  throw an exception
-
-            // If the placeholder contains an ordinal, use it: {2{pages_count}} -> %2$s
-            val match = matchResult.groupValues[0]
-            if (Character.isDigit(match[1])) {
-                "%${match[1]}\$s"
-            } else { // If not, use "1" as the ordinal: {{pages_count}} -> %1$s
-                "%1\$s"
-            }
-        }
-
-        return translationString
-            // Replace % with %% if variables are found
-            .let { if (containsVariables) it.replace("%", "%%") else it }
-            .unescapeHtmlTags()
-            // Replace placeholders from {{variable}} to %1$s format.
-            .replace(VARIABLE_REGEX, placeholderTransform)
-    }
 
     /**
      * Splits an Android XML file to multiple XML files depending on regex matching.
      */
 
-    private fun formatTranslationXmlDocument(
+    private fun processAndCategorizeXmlElements(
         document: Document,
         nodeList: NodeList,
     ): MutableMap<String, MutableList<Element>> {
@@ -97,7 +62,7 @@ class XmlPostProcessor {
                 when (nodeElement.tagName) {
                     TAG_RESOURCES -> {
                         // Main node, traverse its children
-                        val innerMap = formatTranslationXmlDocument(
+                        val innerMap = processAndCategorizeXmlElements(
                             document,
                             nodeElement.childNodes,
                         )
@@ -110,13 +75,14 @@ class XmlPostProcessor {
                         }
                     }
                     TAG_PLURALS -> {
+
                         // Plurals node, process its children
-                        val innerMap = formatTranslationXmlDocument(
+                        val pluralItemsMap = processAndCategorizeXmlElements(
                             document,
                             nodeElement.childNodes,
                         )
 
-                        innerMap.forEach { key, value ->
+                        pluralItemsMap.forEach { key, value ->
                             if (!elemntMap.containsKey(key)) {
                                 elemntMap[key] = mutableListOf<Element>()
                             }
@@ -125,7 +91,7 @@ class XmlPostProcessor {
                     }
                     TAG_STRING -> {
                         // String node, apply transformation to the content
-                        val element = transformElement(nodeElement)
+                        val element = sanitizeAndroidResourceElementAndRecognizeTargetModule(nodeElement)
                         if (!elemntMap.containsKey(element.first)) {
                             elemntMap[element.first] = mutableListOf<Element>()
                         }
@@ -134,7 +100,7 @@ class XmlPostProcessor {
                     }
                     TAG_ITEM -> {
                         // Plurals item node, apply transformation to the content
-                        val element = transformElement(nodeElement)
+                        val element = sanitizeAndroidResourceElementAndRecognizeTargetModule(nodeElement)
                         if (!elemntMap.containsKey(element.first)) {
                             elemntMap[element.first] = mutableListOf<Element>()
                         }
@@ -147,37 +113,4 @@ class XmlPostProcessor {
         return elemntMap
     }
 
-    /**
-     * DEV NOTE: transformation is simplified and thus does not supports CDATA processing!!!
-     */
-    private fun transformElement(
-        nodeElement: Element,
-    ): Pair<String, Element> {
-
-        val copiedNodeElement: Element
-        var moduleName = ""
-
-        val (cDataNode, cDataPosition) = getCDataChildForNode(nodeElement)
-
-        if (cDataNode == null) {
-            val content = nodeElement.textContent
-            val originalNameAttibute = nodeElement.getAttribute(ATTR_NAME)
-            val fixedNameAttribute =
-                originalNameAttibute.replace(ATTR_NAME_IOS_DELIMITER, ATTR_NAME_ANDROID_DELIMITER)
-
-            val processedContent = formatTranslationString(content)
-
-            copiedNodeElement = (nodeElement.cloneNode(true) as Element).apply {
-                textContent = processedContent
-                setAttribute(ATTR_NAME, fixedNameAttribute)
-            }
-            moduleName = getModuleName(originalNameAttibute)
-
-            return Pair(moduleName, copiedNodeElement)
-        } else {
-            logger.warn("CDATA processing in string resources is not supported by this processor yet!")
-            //Check https://github.com/hyperdevs-team/poeditor-android-gradle-plugin how to process CDATA
-            return Pair(moduleName, nodeElement)
-        }
-    }
 }
---
+++ build-logic/convention/src/main/kotlin/cz/kotox/android/poeditor/xml/XmlStringPostProcessor.kt
@@ -0,0 +1,77 @@
+package cz.kotox.android.poeditor.xml
+
+import cz.kotox.android.poeditor.getModuleName
+import org.w3c.dom.Element
+
+private val VARIABLE_REGEX = Regex("""\{\d?\{(.*?)\}\}""")
+
+private const val ATTR_NAME = "name"
+private const val ATTR_NAME_IOS_DELIMITER = "."
+private const val ATTR_NAME_ANDROID_DELIMITER = "."
+
+/**
+ * Transform
+ * DEV NOTE: transformation is simplified and thus does not supports CDATA processing!!!
+ */
+internal fun sanitizeAndroidResourceElementAndRecognizeTargetModule(
+    nodeElement: Element,
+): Pair<String, Element> {
+
+    val copiedNodeElement: Element
+    var moduleName = ""
+
+    val (cDataNode, cDataPosition) = getCDataChildForNode(nodeElement)
+
+    if (cDataNode == null) {
+        val content = nodeElement.textContent
+        val originalNameAttibute = nodeElement.getAttribute(ATTR_NAME)
+        val fixedNameAttribute =
+            originalNameAttibute.replace(
+                ATTR_NAME_IOS_DELIMITER,
+                ATTR_NAME_ANDROID_DELIMITER
+            )
+
+        val processedContent = formatTranslationString(content)
+
+        copiedNodeElement = (nodeElement.cloneNode(true) as Element).apply {
+            textContent = processedContent
+            setAttribute(ATTR_NAME, fixedNameAttribute)
+        }
+        moduleName = getModuleName(originalNameAttibute)
+
+        return Pair(moduleName, copiedNodeElement)
+    } else {
+        logger.warn("CDATA processing in string resources is not supported by this processor yet!")
+        //Check https://github.com/hyperdevs-team/poeditor-android-gradle-plugin how to process CDATA
+        return Pair(moduleName, nodeElement)
+    }
+}
+
+/**
+ * Formats a given string to conform to Android strings.xml format.
+ */
+fun formatTranslationString(translationString: String): String {
+    // We need to check for variables to see if we have to escape percent symbols: if we find variables, we have
+    // to escape them
+    val containsVariables = translationString.contains(VARIABLE_REGEX)
+
+    val placeholderTransform: (MatchResult) -> CharSequence = { matchResult ->
+        // Pending: if the string has multiple variables but any of them has no order number,
+        //  throw an exception
+
+        // If the placeholder contains an ordinal, use it: {2{pages_count}} -> %2$s
+        val match = matchResult.groupValues[0]
+        if (Character.isDigit(match[1])) {
+            "%${match[1]}\$s"
+        } else { // If not, use "1" as the ordinal: {{pages_count}} -> %1$s
+            "%1\$s"
+        }
+    }
+
+    return translationString
+        // Replace % with %% if variables are found
+        .let { if (containsVariables) it.replace("%", "%%") else it }
+        .unescapeHtmlTags()
+        // Replace placeholders from {{variable}} to %1$s format.
+        .replace(VARIABLE_REGEX, placeholderTransform)
+}
