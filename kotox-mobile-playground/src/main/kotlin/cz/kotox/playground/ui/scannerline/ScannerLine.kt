8b2424273ff7678300a2131c04a72c9ba02420db
Michal <jenicek.michal@gmail.com>
2023-05-14 22:37:34 +0200

    Introduce playground skeleton for scanner line animation.

---
+++ kotox-mobile-playground/src/main/kotlin/cz/kotox/playground/ui/scannerline/ScannerLine.kt
@@ -0,0 +1,108 @@
+package cz.kotox.playground.ui.scannerline
+
+import android.content.res.Configuration
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import cz.kotox.core.ui.theme.KotoxBasicTheme
+
+@Composable
+fun ScannerLine(
+    boxSize: Dp = 172.dp,
+    verticalAnimationDuration: Int = 2000,
+    color: Color = Color.Red,
+    thickness: Dp = 2.dp,
+) {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val heightAnimation: Dp by infiniteTransition.animateValue(
+        initialValue = 0.dp,
+        targetValue = boxSize,
+        typeConverter = Dp.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = verticalAnimationDuration
+                0.dp at 0 // ms
+                0.dp at 0 + 300 // ms
+
+//                80.dp at 300 with FastOutLinearInEasing
+                boxSize at (verticalAnimationDuration / 2) - 150 with FastOutSlowInEasing
+                boxSize at (verticalAnimationDuration / 2) + 150
+
+                0.dp at verticalAnimationDuration - 300 with FastOutSlowInEasing
+                0.dp at verticalAnimationDuration
+            }
+            // Use the default RepeatMode.Restart to start from 0.dp after each iteration
+        )
+    )
+
+    val widthAnimation: Dp by infiniteTransition.animateValue(
+        initialValue = 0.dp,
+        targetValue = boxSize,
+        typeConverter = Dp.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 2 * verticalAnimationDuration
+                0.dp at 0 //ms
+                0.dp at 150 //ms
+                0.dp at verticalAnimationDuration - 150
+                150.dp at verticalAnimationDuration
+                150.dp at verticalAnimationDuration*2 -150
+                0.dp at verticalAnimationDuration*2
+            }
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .size(boxSize),
+//            .border(1.dp, Color(0xFF000000), RoundedCornerShape(2.dp)),
+    ) {
+//            color = MaterialTheme.colorScheme.background // Set background color
+//        Image(
+//            painter = painterResource(id = R.drawable.datamatrix_jojo),
+//            contentDescription = "start scanning",
+//            modifier = Modifier.size(boxSize),
+//        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(heightAnimation))
+            Divider(
+                thickness = thickness,
+                color = color,
+                modifier = Modifier.width(widthAnimation)
+            )
+        }
+    }
+}
+
+@Preview(
+    device = Devices.PIXEL,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun ScannerLinePreview() {
+    KotoxBasicTheme() {
+        ScannerLine()
+    }
+}
---
+++ kotox-mobile-playground/src/main/kotlin/cz/kotox/playground/ui/scannerline/ScannerLineActivity.kt
@@ -0,0 +1,27 @@
+package cz.kotox.playground.ui.scannerline
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import cz.kotox.core.ui.theme.KotoxBasicTheme
+
+class ScannerLineActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotoxBasicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ScannerLine()
+                }
+            }
+        }
+    }
+}
