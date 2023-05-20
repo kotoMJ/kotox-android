eeca0341b1a45fcf3c82c9a91f40fcb9f002a1a7
Michal <jenicek.michal@gmail.com>
2023-05-20 23:10:02 +0200

    Introduce text common module with basic rich text extension functions.

---
+++ common-text/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("cz.kotox.android.library")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+}
+
+dependencies {
+    implementation(projects.common)
+
+    implementation(libs.androidx.hilt.android)
+    kapt(libs.androidx.hilt.compiler)
+
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.core)
+
+    implementation(libs.timber)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+}
