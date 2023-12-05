f1f0f35deba359cbc0d4be6e88f5c51de7efbd81
Michal <jenicek.michal@gmail.com>
2023-12-05 21:15:42 +0100

    Add ScreenCaptureLock compose code.

--- common/camera/src/main/kotlin/cz/kotox/common/camera/custom/files/FileUtils.kt
+++ common/camera/src/main/kotlin/cz/kotox/common/camera/custom/files/FileUtils.kt
@@ -1,43 +1,43 @@
 package cz.kotox.common.camera.custom.files
 
-import android.content.Context
-import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import androidx.core.content.FileProvider
-import timber.log.Timber
-import java.io.File
+//import android.content.Context
+//import android.net.Uri
+//import android.os.Build
+//import android.os.Environment
+//import androidx.core.content.FileProvider
+//import timber.log.Timber
+//import java.io.File
 
-object FileUtils {
-    /**
-     * Get URI to image received from capture by camera.
-     * URI location: sdcard/Android/data/com.aisense.otter.../files/Pictures/pickImageResult12345.jpeg
-     *
-     * @param context used to access Android APIs, like content resolve, it is your
-     * activity/fragment/widget.
-     */
-    @SuppressWarnings("TooGenericExceptionCaught")
-    fun getCameraImageOutputUri(context: Context): Uri? {
-        val outputFileUri: Uri?
-        val getImage: File?
-
-        // We have this because of a HUAWEI path bug when we use getUriForFile
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            getImage = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            outputFileUri = try {
-                FileProvider.getUriForFile(
-                    context,
-                    /*BuildConfig.FILE_PROVIDER_AUTHORITY*/"cz.kotox.camera.custom", //FIXME MJ - add file provider authority
-                    File.createTempFile("pickImageResult", ".jpeg", getImage)
-                )
-            } catch (e: Exception) {
-                Timber.w(e, "Fallback for image uri")
-                Uri.fromFile(File(getImage!!.path, "pickImageResult.jpeg"))
-            }
-        } else {
-            getImage = context.externalCacheDir
-            outputFileUri = Uri.fromFile(File(getImage!!.path, "pickImageResult.jpeg"))
-        }
-        return outputFileUri
-    }
-}
+//object FileUtils {
+//    /**
+//     * Get URI to image received from capture by camera.
+//     * URI location: sdcard/Android/data/com.aisense.otter.../files/Pictures/pickImageResult12345.jpeg
+//     *
+//     * @param context used to access Android APIs, like content resolve, it is your
+//     * activity/fragment/widget.
+//     */
+//    @SuppressWarnings("TooGenericExceptionCaught")
+//    fun getCameraImageOutputUri(context: Context): Uri? {
+//        val outputFileUri: Uri?
+//        val getImage: File?
+//
+//        // We have this because of a HUAWEI path bug when we use getUriForFile
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            getImage = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+//            outputFileUri = try {
+//                FileProvider.getUriForFile(
+//                    context,
+//                    /*BuildConfig.FILE_PROVIDER_AUTHORITY*/"cz.kotox.camera.custom", //FIXME MJ - add file provider authority
+//                    File.createTempFile("pickImageResult", ".jpeg", getImage)
+//                )
+//            } catch (e: Exception) {
+//                Timber.w(e, "Fallback for image uri")
+//                Uri.fromFile(File(getImage!!.path, "pickImageResult.jpeg"))
+//            }
+//        } else {
+//            getImage = context.externalCacheDir
+//            outputFileUri = Uri.fromFile(File(getImage!!.path, "pickImageResult.jpeg"))
+//        }
+//        return outputFileUri
+//    }
+//}
--- common/ui/src/main/kotlin/cz/kotox/common/ui/compose/screencapture/ScreenCaptureLock.kt
+++ common/ui/src/main/kotlin/cz/kotox/common/ui/compose/screencapture/ScreenCaptureLock.kt
@@ -1,117 +1,83 @@
 package cz.kotox.common.ui.compose.screencapture
 
-//FIXME MJ - uncomment this with targeting Android 14
+import android.app.Activity
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import cz.kotox.common.core.android.extension.disableScreenCapture
+import cz.kotox.common.core.android.extension.enableScreenCapture
 
-//import android.app.Activity
-//import android.os.Build
-//import androidx.annotation.RequiresApi
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.DisposableEffect
-//import androidx.compose.runtime.LaunchedEffect
-//import androidx.compose.runtime.remember
-//import com.google.accompanist.permissions.ExperimentalPermissionsApi
-//import com.google.accompanist.permissions.rememberPermissionState
-//import cz.kotox.common.core.android.extension.disableScreenCapture
-//import cz.kotox.common.core.android.extension.enableScreenCapture
-//import cz.kotox.common.core.android.permission.getReadMediaImagesPermission
-//import cz.kotox.common.core.android.screencapture.LegacyScreenCaptureDetectionDelegate
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import kotlinx.coroutines.FlowPreview
-//import timber.log.Timber
-//
-//@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-//@OptIn(
-//    FlowPreview::class,
-//    ExperimentalCoroutinesApi::class, ExperimentalPermissionsApi::class, ExperimentalPermissionsApi::class
-//)
-//@Composable
-//internal fun ScreenCaptureLock(
-//    screenAllowedCondition: () -> Boolean,
-//    onScreenCaptured: () -> Unit,
-//    onScreenPotentiallyCaptured: () -> Unit,
-//    activity: Activity?
-//) {
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-//        val screenCaptureDetectionDelegate = remember {
-//            Activity.ScreenCaptureCallback {
-//                onScreenCaptured.invoke()
-//            }
-//        }
-//
-//        DisposableEffect(
-//            screenAllowedCondition,
-//            activity?.window
-//        ) {
-//            val window = activity?.window
-//            if (screenAllowedCondition()) {
-//                window?.disableScreenCapture()
-//                activity?.registerScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
-//            } else {
-//                activity?.unregisterScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
-//                window?.enableScreenCapture()
-//            }
-//            onDispose {
-//                activity?.unregisterScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
-//                window?.enableScreenCapture()
-//            }
-//        }
-//    } else {
-//        val legacyScreenCaptureDetectionDelegate: LegacyScreenCaptureDetectionDelegate? = remember {
-//            activity?.let {
-//                Timber.d(">>>_ ScreenshotDetectionDelegate")
-//                LegacyScreenCaptureDetectionDelegate(
-//                    it,
-//                    object : LegacyScreenCaptureDetectionDelegate.ScreenshotDetectionListener {
-//                        override fun onScreenCaptured(path: String) {
-//                            Timber.d(">>>_ ScreenshotDetectionDelegate")
-//                            onScreenCaptured.invoke()
-//                        }
-//
-//                        override fun onPotentialScreenCaptured() {
-//                            Timber.w(">>>_ Request permissions, screen is  potentially captured")
-//                            onScreenPotentiallyCaptured.invoke()
-//                        }
-//                    }
-//                )
-//            }
-//        }
-//        val permissionState = rememberPermissionState(permission = getReadMediaImagesPermission())
-//        LaunchedEffect(permissionState) {
-//            permissionState.launchPermissionRequest()
-//        }
-//
-//        DisposableEffect(
-//            screenAllowedCondition,
-//            activity?.window
-//        ) {
-//            val window = activity?.window
-//            if (screenAllowedCondition()) {
-//                window?.disableScreenCapture()
-//                legacyScreenCaptureDetectionDelegate?.startScreenshotDetection()
-//            } else {
-//                legacyScreenCaptureDetectionDelegate?.stopScreenshotDetection()
-//                window?.enableScreenCapture()
-//            }
-//            onDispose {
-//                legacyScreenCaptureDetectionDelegate?.stopScreenshotDetection()
-//                window?.enableScreenCapture()
-//            }
-//        }
-//    }
-//}
-//
-//private fun Activity.unregisterScreenCaptureCallbackSafe(callback: Activity.ScreenCaptureCallback) {
-//    try {
-//        unregisterScreenCaptureCallback(callback)
-//    } catch (notRegisteredExteption: IllegalStateException) {
-//        /* It's not a problem when capture callback is not registered */
-//    }
-//}
-//
-//private fun Activity.registerScreenCaptureCallbackSafe(callback: Activity.ScreenCaptureCallback) {
-//    try {
-//        registerScreenCaptureCallback(mainExecutor, callback)
-//    } catch (alreadyRegisteredException: IllegalStateException) {
-//        /* It's not a problem when capture callback is already registered */
-//    }
-//}
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+@Composable
+internal fun ScreenCaptureLock(
+    screenAllowedCondition: () -> Boolean,
+    onScreenCaptured: () -> Unit,
+    activity: Activity?
+) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        val screenCaptureDetectionDelegate = remember {
+            Activity.ScreenCaptureCallback {
+                onScreenCaptured.invoke()
+            }
+        }
+
+        DisposableEffect(
+            screenAllowedCondition,
+            activity?.window
+        ) {
+            val window = activity?.window
+            if (screenAllowedCondition()) {
+                window?.disableScreenCapture()
+                activity?.registerScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
+            } else {
+                activity?.unregisterScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
+                window?.enableScreenCapture()
+            }
+            onDispose {
+                activity?.unregisterScreenCaptureCallbackSafe(screenCaptureDetectionDelegate)
+                window?.enableScreenCapture()
+            }
+        }
+    } else {
+
+        /**
+         * LegacyScreenCapture detection is not implemented on purpose. The hack with observing file storage
+         * is too error prone (and therefore risky). It would be also a lot of legacy code to be removed in the future.
+         * Let's wait with backwards compatible screen capture detection for official API.
+         */
+        DisposableEffect(
+            screenAllowedCondition,
+            activity?.window
+        ) {
+            val window = activity?.window
+            if (screenAllowedCondition()) {
+                window?.disableScreenCapture()
+            } else {
+                window?.enableScreenCapture()
+            }
+            onDispose {
+                window?.enableScreenCapture()
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+private fun Activity.unregisterScreenCaptureCallbackSafe(callback: Activity.ScreenCaptureCallback) {
+    try {
+        unregisterScreenCaptureCallback(callback)
+    } catch (notRegisteredExteption: IllegalStateException) {
+        /* It's not a problem when capture callback is not registered */
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+private fun Activity.registerScreenCaptureCallbackSafe(callback: Activity.ScreenCaptureCallback) {
+    try {
+        registerScreenCaptureCallback(mainExecutor, callback)
+    } catch (alreadyRegisteredException: IllegalStateException) {
+        /* It's not a problem when capture callback is already registered */
+    }
+}
